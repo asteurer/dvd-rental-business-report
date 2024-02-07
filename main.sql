17e57a75ee59306687f0b7cea14725ae28ac03e6
Andrew Steurer <andrew.w.steurer@gmail.com>
2024-02-07 11:14:03 -0700

    combining .sql files

---
+++ main.sql
@@ -0,0 +1,65 @@
+--TODO: Update the date comparisons in the WHERE clause to have CURRENT_DATE rather than 05/01/2005
+
+CREATE OR REPLACE FUNCTION generate_summary_report() 
+	 RETURNS TRIGGER
+	 LANGUAGE PLPGSQL
+AS $$
+BEGIN
+	INSERT INTO summary_report (
+		last_day_of_month,
+		store_id,
+		category_name,
+		potential_sales,
+		total_rentals
+	)
+	SELECT
+		(DATE_TRUNC('MONTH', detailed_report.rental_date) + INTERVAL '1 MONTH' - INTERVAL '1 day')::DATE as last_day_of_month,
+		detailed_report.store_id,
+		detailed_report.category_name,
+		CONCAT('$', SUM(detailed_report.rental_rate * detailed_report.rental_duration)) AS potential_sales,
+		COUNT(detailed_report.rental_id) AS total_rentals
+	WHERE date_part('month', rental.rental_date) = date_part('month', '05/01/2005'::DATE)
+		AND date_part('year', rental.rental_date) = date_part('year',  '05/01/2005'::DATE)
+	GROUP BY last_day_of_month, staff.store_id, category.name;
+RETURN NEW;
+END; 
+$$;
+
+
+CREATE OR REPLACE FUNCTION generate_detailed_report() 
+	RETURNS TRIGGER
+	LANGUAGE PLPGSQL
+AS $$
+BEGIN
+	INSERT INTO detailed_report(
+		rental_id,
+		rental_date,
+		rental_rate,
+		rental_duration,
+		store_id,
+		category_name
+	)
+	SELECT 
+		rental.rental_id, 
+		rental.rental_date, 
+		film.rental_rate,
+		film.rental_duration,
+		staff.store_id, 
+		category.name AS category_name
+	FROM
+		rental
+	LEFT JOIN inventory 
+		ON rental.inventory_id = inventory.inventory_id
+	LEFT JOIN film_category
+		ON inventory.film_id = film_category.film_id
+	LEFT JOIN category
+		ON film_category.category_id = category.category_id
+	LEFT JOIN film
+		ON inventory.film_id = film.film_id
+	LEFT JOIN staff
+		ON rental.staff_id = staff.staff_id
+	WHERE date_part('month', rental.rental_date) = date_part('month', '05/01/2005'::DATE)
+		AND date_part('year', rental.rental_date) = date_part('year', '05/01/2005'::DATE); 
+RETURN NEW; 
+END; 
+$$;
